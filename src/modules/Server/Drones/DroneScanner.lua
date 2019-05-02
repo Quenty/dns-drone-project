c03b8b1b0378ac7bbb0f54c820255a00dc9f0bd0
James Onnen <jonnen0@gmail.com>
2019-05-02 14:45:51 -0500

    Fix scanning

--- src/modules/Server/Drones/DroneScanner.lua
+++ src/modules/Server/Drones/DroneScanner.lua
@@ -11,9 +11,10 @@ local BaseObject = require("BaseObject")
 local Draw = require("Draw")
 local Raycaster = require("Raycaster")
 
-local DESIRED_HEIGHT = 20
+local DEBUG_SCANS = false
+
 local SCAN_UP_FROM = 25
-local SCAN_LENGTH = SCAN_UP_FROM + DESIRED_HEIGHT
+local SCAN_COUNT = 75
 
 local DroneScanner = setmetatable({}, BaseObject)
 DroneScanner.ClassName = "DroneScanner"
@@ -32,23 +33,34 @@ function DroneScanner.new(obj)
 	return self
 end
 
-function DroneScanner:ScanInFront(position, velocity)
+function DroneScanner:ScanInFront(position, velocity, height)
 	local hits = {}
-	local direction = velocity.unit * Vector3.new(1, 0, 1)
+	local scanLength = height + SCAN_UP_FROM
 
-	for i=0, 50, 1 do
-		local pos = position + direction*i + Vector3.new(0, SCAN_UP_FROM, 0)
-		local ray = Ray.new(pos, Vector3.new(0, -SCAN_LENGTH, 0))
-
-
-		local hitData = self._raycaster:FindPartOnRay(ray)
-		if hitData then
-			Debris:AddItem(Draw.Ray(ray), 0.05)
-			table.insert(hits, hitData)
+	if (math.abs(velocity.y) + math.abs(velocity.z)) == 0 then
+		local ray = Ray.new(position, Vector3.new(0, -scanLength, 0))
+		self:_doRayScan(hits, ray)
+	else
+		local direction = (velocity * Vector3.new(1, 0, 1)).unit
+		for i=0, SCAN_COUNT, 1 do
+			local pos = position + direction*i + Vector3.new(0, SCAN_UP_FROM, 0)
+			local ray = Ray.new(pos, Vector3.new(0, -scanLength, 0))
+			self:_doRayScan(hits, ray)
 		end
 	end
 
 	return hits
+end
+
+function DroneScanner:_doRayScan(hits, ray)
+	local hitData = self._raycaster:FindPartOnRay(ray)
+	if hitData then
+		if DEBUG_SCANS then
+			Debris:AddItem(Draw.Ray(ray), 0.05)
+		end
+
+		table.insert(hits, hitData)
+	end
 end
 
 return DroneScanner
